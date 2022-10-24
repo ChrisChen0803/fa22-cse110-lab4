b60870855e071811ee0928907d0639e43592693d
ChrisChen0803 <78402800+ChrisChen0803@users.noreply.github.com>
2022-10-23 19:28:24 -0700

    Update part2-question18.js

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -1,6 +1,6 @@
-let d = new Date();
-let time = d.toLocaleTimeString();
 setInterval(pt,"1000");
 function pt(){
+    let d = new Date();
+    let time = d.toLocaleTimeString();
     console.log(time);
 }
